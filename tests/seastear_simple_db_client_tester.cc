862140740c6472745616db4bc9585a84108fe25a
forestgril2 <forestgril2@github.com>
2024-10-11 20:28:16 +0000

    Simplify the client tester further.

--- tests/seastear_simple_db_client_tester.cc
+++ tests/seastear_simple_db_client_tester.cc
@@ -47,45 +47,23 @@ struct printer {
 
 int main(int ac, char** av) {
     app_template app;
-    app.add_options()
-            ("host", bpo::value<std::string>(), "Host to connect")
-            ("path", bpo::value<std::string>(), "Path to query upon")
-            ("method", bpo::value<std::string>()->default_value("GET"), "Method to use")
-            ("file", bpo::value<std::string>(), "File to get body from (no body if missing)")
-    ;
 
     return app.run(ac, av, [&] {
 
         auto&& config = app.configuration();
-        auto host = config["host"].as<std::string>();
-        auto path = config["path"].as<std::string>();
-        auto method = config["method"].as<std::string>();
-        auto body = config.count("file") == 0 ? std::string("") : config["file"].as<std::string>();
+        auto host = std::string("localhost");
+        auto path = std::string("/");
+        auto method = std::string("GET");
 
         return seastar::async([=] {
             net::hostent e = net::dns::get_host_by_name(host, net::inet_address::family::INET).get();
             std::unique_ptr<http::experimental::client> cln;
-
-            fmt::print("{} {}:10000{}\n", method, e.addr_list.front(), path);
 
             auto addr = e.addr_list.front();
             auto address = socket_address(addr, 10000);
             cln = std::make_unique<http::experimental::client>(address);
 
             auto req = http::request::make(method, host, path);
-            if (body != "") {
-
-                future<file> f = open_file_dma(body, open_flags::ro);
-                req.write_body("txt", [ f = std::move(f) ] (output_stream<char>&& out) mutable {
-                    return seastar::async([f = std::move(f), out = std::move(out)] () mutable {
-                        auto in = make_file_input_stream(f.get());
-                        copy(in, out).get();
-                        out.flush().get();
-                        out.close().get();
-                        in.close().get();
-                    });
-                });
-            }
 
             cln->make_request(std::move(req), [] (const http::reply& rep, input_stream<char>&& in) {
                 fmt::print("Reply status {}\n--------8<--------\n", rep._status);
